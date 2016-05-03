531518698dd0eea4f0ac3a722380ff242f6c433c
Dianalim209 <dianamlim209@yahoo.com>
2016-05-02 19:05:09 -0700

    Added files via upload

---
+++ multiply.py
@@ -0,0 +1,8 @@
+#create a function that returns values multiplied by 5
+def multiply(a,b):
+	x = a * b
+	return x
+
+a = [2,4,10,16]
+result = multiply(a,5)
+print result
---
+++ odd_even.py
@@ -0,0 +1,7 @@
+#create a function that counts from 1 to 2000
+for count in range(1,2001):
+	if count % 2 == 0:
+		print "Number is", count, "This is an even number."
+	elif count % 2 == 1:
+		print "Number is", count, "This is an odd number."
+# defining the odd or even number
