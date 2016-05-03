4318a39b7af5dba74c02914e761d5e0a5027ce41
Dianalim209 <dianamlim209@yahoo.com>
2016-05-02 18:30:29 -0700

    Added files via upload

---
+++ average.py
@@ -0,0 +1,5 @@
+#create a program that prints the average of the values in the list"
+a = [1,2,5,10,255,3]
+b = sum(a)
+count = b/len(a)
+print count 
---
+++ sum_list.py
@@ -0,0 +1,5 @@
+#creating a sum list
+
+a = [1,2,5,10,255,3]
+count = sum(a)
+print count
