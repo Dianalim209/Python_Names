92d78860541438737f83b807a48f196d34c0f1a9
Dianalim209 <dianamlim209@yahoo.com>
2016-05-02 17:58:11 -0700

    creating names folder

---
+++ demo.py
@@ -0,0 +1,5 @@
+myVariable = ["phrase1", "phrase2", 37]
+# myVariable[0] = "Diana"
+myVariable2 = ("banana", "flower", 42)
+myDecisionMaker = ("decision1", "decision2")
+print myVariable[0]
---
+++ multiples.py
@@ -0,0 +1,11 @@
+#Create a program that prints all the odd numbers from 1 to 1000
+
+for i in range(0,1000):
+	if i % 2 == 1:
+		print i
+
+
+#Create another program that prints all the multiples of 5 from 5 to 1,000,000. 
+for i in range(5,1000001):
+	if i % 5 == 0:
+		print i
